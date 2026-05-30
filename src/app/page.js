73e7d5ff2fac36fa28b48e502466f5f09ccfe447
TwinkleMedia <hs0052264@gmail.com>
2026-05-30 14:58:48 +0530

    Dontion 4

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,4 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   return redirect("/admin/login");
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 3fc70c2108e4d36b58c30fce2124e45f8c7b6821
